d9b71f8f98be198abc4b1bfa6142574b049f33c9
Tabia Nadir <nadirumer786001@gmail.com>
2025-02-24 23:17:52 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,6 @@
+def greet():
+    name = input("Enter Your Name: ")
+    massage = f"Hello, Python Learners! Today is the first step in our coding journey.\n- {name}"
+    print(massage)
+
+greet()  
